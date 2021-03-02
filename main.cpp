9b4c335b8aea9350d19c74085729236b3ccc81dd
newpolaris <newpolaris@gmail.com>
2021-03-02 23:30:51 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
+#include "glad.h"
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+#include "Application.h"
+
+void glfw_error_callback(int error, const char* description)
+{
+    std::cerr << "Error: " << description << std::endl;
+}
 
 int main(void)
 {
     GLFWwindow* window;
 
+    glfwSetErrorCallback(glfw_error_callback);
+
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Animation Sample", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -18,21 +35,56 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    
+    GLuint gVertexArrayObject = 0;
+    glGenVertexArrays(1, &gVertexArrayObject);
+    glBindVertexArray(gVertexArrayObject);
+
+    Application* gApplication = new Application();
+    gApplication->Initialize();
+
+    glClearColor(0.4f, 0.3f, 0.4f, 0.0f);
+
+    float lastTime = glfwGetTime();
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        float now = glfwGetTime();
+        float dt = now - lastTime;
+
+        int clientWidth, clientHeight;
+		glfwGetWindowSize(window, &clientWidth, &clientHeight);
+
+        if (gApplication != 0) {
+            gApplication->Update(dt);
+        }
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindVertexArray(gVertexArrayObject);
+
+        float aspect = (float)clientWidth / (float)clientHeight;
+        if (gApplication != 0) {
+            gApplication->Render(aspect);
+        }
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
+
+        lastTime = now;
     }
 
+    delete gApplication;
+    gApplication = 0;
+
+    glBindVertexArray(0);
+    glDeleteVertexArrays(1, &gVertexArrayObject);
+    gVertexArrayObject = 0;
+    
     glfwTerminate();
     return 0;
 }
